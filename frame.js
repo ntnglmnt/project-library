45c1d5f476a20a4f94c18b055113b42440421da6
doomsday <intniogi@hotmail.com>
2026-06-02 11:53:57 +0300

    created library array, created book constructor, created storeToLibrary function that created and stores books

--- frame.js
+++ frame.js
@@ -1,16 +1,16 @@
-function Book(title, author, pages, read) {
-    if (!new.target) {
-        throw Error("You must use the new operator to call the constructor")
-    }
-    this.title = title;
-    this.author = 'by ' + author;
-    this.pages = pages +' pages';
-    this.read = read;
-    this.info = function () {
-        console.log(this.title, this.author, this.pages, this.read)
-    }
-}
+const myLibrary = [];
 
-bookOne = new Book('The Hobbit', 'J.J.R Tolkien', '295', 'aready read');
+function Book(arr) {
+    // the constructor
 
-bookOne.info()
+    this.title = arr[0];
+    this.author = arr[1];
+    this.pages = arr[2];
+    this.read = arr[3];
+    this.id = crypto.randomUUID();
+};
+
+function storeToLibrary(title, author, pages, read, array) {
+    // take params, create a book and then store it in the array
+    
+};
