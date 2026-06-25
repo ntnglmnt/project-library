43e20b7876121df95e0b399972acfbb0657d0f00
doomsday <intniogi@hotmail.com>
2026-06-25 13:51:46 +0300

    book is now a class, library is an object for methods, library parts are created with func createLibraryParts

--- frame.js
+++ frame.js
@@ -93,94 +93,6 @@
 // add.addEventListener('click', storeToLibrary)
 // document.body.appendChild(add)
 
-const library = {
-
-    mylibrary: [],
-
-    storeToLibrary: function (title, author, pages, read) {
-
-        title = titleInput.value
-        author = authorInput.value
-        pages = pagesInput.value
-        read = readInput.value
-
-        let newBook = new Book(title, author, pages, read)
-        console.log(newBook)
-        library.mylibrary.push(newBook)
-        console.log(library.mylibrary)
-    },
-
-    initializeLibrary: function () {
-
-        let lib = library.mylibrary
-
-        function bookDisplay(arr) {
-
-            arr.forEach((item) => {
-
-                let bookSpace = document.createElement('div')
-                console.log(bookSpace)
-                let content = item.value
-                bookSpace.innerHTML = content
-                display.appendChild(bookSpace)
-
-                let removeBookSpace = document.createElement('button')
-                removeBookSpace.classList.add('removeBookSpaceBtn')
-                removeBookSpace.textContent = 'Remove'
-                removeBookSpace.addEventListener('click', () => {
-                    bookSpace.innerText = ''
-                    displayBox.removeChild(removeBookSpace)
-                })
-                bookSpace.appendChild(removeBookSpace)
-
-                let toggleRead = document.createElement('button')
-                toggleRead.classList.add('checkBtn')
-                toggleRead.textContent = 'Read'
-                toggleRead.addEventListener('click', () => {
-                    book.read = 'read'
-                    bookSpace.innerText = book.showBook()
-                    bookSpace.appendChild(removeBookSpace)
-                    bookSpace.appendChild(toggleRead)
-                    bookSpace.appendChild(toggleNotRead)
-                })
-                bookSpace.appendChild(toggleRead)
-                let toggleNotRead = document.createElement('button')
-                toggleNotRead.classList.add('checkBtn')
-                toggleNotRead.textContent = 'not read'
-                toggleNotRead.addEventListener('click', () => {
-                    book.read = 'Not read'
-                    console.log(book)
-                    bookSpace.innerText = book.showBook()
-                    bookSpace.appendChild(removeBookSpace)
-                    bookSpace.appendChild(toggleRead)
-                    bookSpace.appendChild(toggleNotRead)
-                })
-                bookSpace.appendChild(toggleNotRead)
-            })
-        }
-
-        const display = document.createElement('div')
-        display.classList.add('display')
-        document.body.appendChild(display);
-
-        const displayBtn = document.createElement('button')
-        displayBtn.classList.add('displayBtn')
-        displayBtn.textContent = 'Display'
-        displayBtn.addEventListener('click', bookDisplay(lib))
-        display.appendChild(displayBtn)
-
-        const titleInput = document.getElementById('titleInput')
-        const authorInput = document.getElementById('authorInput')
-        const pagesInput = document.getElementById('pagesInput')
-        const readInput = document.getElementById('readInput')
-
-        const add = document.createElement('button')
-        add.textContent = 'Add'
-        add.addEventListener('click', library.storeToLibrary)
-        document.body.appendChild(add)
-    }
-}
-
 class Book {
 
     constructor(title, author, pages, read, id) {
@@ -196,6 +108,57 @@ class Book {
     };
 };
 
+const library = {
 
-let initiateLibrary = library.initializeLibrary
-initiateLibrary()
+    libraryArray: [],
+
+    storeBook: function () {
+
+        const titleInput = document.getElementById('titleInput')
+        const authorInput = document.getElementById('authorInput')
+        const pagesInput = document.getElementById('pagesInput')
+        const readInput = document.getElementById('readInput')
+
+        let newBook = new Book(titleInput, authorInput, pagesInput, readInput)
+        library.libraryArray.push(newBook)
+        console.log(this.libraryArray)
+    },
+
+    displayBook: function () {
+        library.libraryArray.map((item) => {
+            item.showBook()
+        })
+    },
+
+    removeBook: function () {
+
+    },
+
+    toggleRead: function () {
+
+    }
+
+}
+
+function createLibraryParts() {
+
+    const display = document.createElement('div')
+    display.classList.add('display')
+    display.textContent = ''
+    document.body.appendChild(display);
+
+    const add = document.createElement('button')
+    add.textContent = 'Add'
+    add.addEventListener('click', library.storeBook)
+    document.body.appendChild(add)
+
+    const displayBtn = document.createElement('button')
+    displayBtn.classList.add('displayBtn')
+    displayBtn.textContent = 'Display'
+    displayBtn.addEventListener('click', library.displayBook)
+    display.appendChild(displayBtn)
+}
+
+let libraryInitialisation = createLibraryParts
+libraryInitialisation()
+
